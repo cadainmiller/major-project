c0c0df0bbd93f14d2b4ec267dee73f8f85cbc1a1
Cadain Miller <cadainmiller@gmail.com>
2020-07-20 20:52:17 -0500

    Reset from completed also completed logic needs to be added.

--- src/app/authentication/reset/reset.component.ts
+++ src/app/authentication/reset/reset.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset',
@@ -8,6 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class ResetComponent implements OnInit {
 
   constructor() { }
+
+  submitted = false;
+
+  resetForm = new FormGroup({
+    email: new FormControl('', Validators.required,)
+  })
+
+  get email() {
+    return this.resetForm.get('email');
+  }
+
+  resetpassword() {
+
+    var getEmail = this.resetForm.value.email;
+
+    console.log(getEmail);
+  }
 
   ngOnInit(): void {
   }
